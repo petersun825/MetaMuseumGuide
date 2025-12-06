bcb9a22535796a7c7c1f17dd36caebd9b0b795e6
petersun825 <petersun825@gmail.com>
2025-12-05 22:43:28 -0500

    Initial Commit

---
+++ MetaMuseumGuide/MetaMuseumGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMuseumGuideApp.swift
+//  MetaMuseumGuide
+//
+//  Created by Peter Sun on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MetaMuseumGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetaMuseumGuideTests/MetaMuseumGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMuseumGuideTests.swift
+//  MetaMuseumGuideTests
+//
+//  Created by Peter Sun on 12/5/25.
+//
+
+import Testing
+@testable import MetaMuseumGuide
+
+struct MetaMuseumGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
